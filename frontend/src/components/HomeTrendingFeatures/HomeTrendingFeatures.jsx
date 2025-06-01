e198782de133f5412036bee67db0c1a62921ba32
Kuba <272264@student.pwr.edu.pl>
2025-06-01 16:18:48 +0200

    Added HomeTrendingFeatures components

---
+++ frontend/src/components/HomeTrendingFeatures/HomeTrendingFeatures.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./homeTrendingFeatures.css";
+
+const HomeTrendingFeatures = () => {
+  return (
+    <div>HomeTrendingFeatures</div>
+  )
+}
+
+export default HomeTrendingFeatures
