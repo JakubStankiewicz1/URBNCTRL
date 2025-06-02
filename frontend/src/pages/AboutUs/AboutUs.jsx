6efee0de288142d52c2035b01d24fd589ad208ba
Kuba <272264@student.pwr.edu.pl>
2025-06-02 23:32:40 +0200

    Added styling to AboutUsInfo component

--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./aboutUs.css";
 import AboutUsHero from '../../components/AboutUsHero/AboutUsHero';
+import AboutUsInfo from '../../components/AboutUsInfo/AboutUsInfo';
 
 const AboutUs = () => {
   return (
     <div className='aboutUs'>
         <AboutUsHero />
+        <AboutUsInfo />
     </div>
   )
 }
