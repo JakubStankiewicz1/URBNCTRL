01553992177cc0520dcdf743aa5094578186ce99
Kuba <272264@student.pwr.edu.pl>
2025-06-02 21:31:40 +0200

    Added homeFeaturedProducts

---
+++ frontend/src/components/AboutUsHero/AboutUsHero.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./aboutUsHero.css";
+
+const AboutUsHero = () => {
+  return (
+    <div className='aboutUsHero'>
+        <div className="aboutUsHeroContainer">
+            <div className="aboutUsHeroContainerDiv">                {/* Info */}
+                <div className="aboutUsHeroContainerDivInfo">
+                    <div className="aboutUsHeroContainerDivInfoBorders"></div>
+                    <div className="aboutUsHeroContainerDivInfoContainer">
+                        <div className="aboutUsHeroContainerDivInfoContainerOverground">
+                            <div className="aboutUsHeroContainerDivInfoContainerOvergroundContainer">
+                                <p className="aboutUsHeroContainerDivInfoContainerOvergroundContainerText">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam voluptate tempore autem corrupti tempora qui nisi. Accusantium labore deleniti officia eos, repudiandae minima odio rem consequatur aliquam. Facilis, inventore nostrum?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutUsHero
--- frontend/src/pages/AboutUs/AboutUs.jsx
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import "./aboutUs.css";
+import AboutUsHero from '../../components/AboutUsHero/AboutUsHero';
 
 const AboutUs = () => {
   return (
-    <div className='aboutUs'>AboutUs</div>
+    <div className='aboutUs'>
+        <AboutUsHero />
+    </div>
   )
 }
 
