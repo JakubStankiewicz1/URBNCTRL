32d1cce1383ba5601372ede3297fc6b64ee1535e
Kuba <272264@student.pwr.edu.pl>
2025-06-03 19:32:53 +0200

    Finished ShippingAndReturns page

--- frontend/src/pages/ShippingAndReturns/ShippingAndReturns.jsx
+++ frontend/src/pages/ShippingAndReturns/ShippingAndReturns.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./shippingAndReturns.css";
 import ShippingAndReturnsHero from '../../components/ShippingAndReturnsHero/ShippingAndReturnsHero';
 import ShippingAndReturnsInfo from '../../components/ShippingAndReturnsInfo/ShippingAndReturnsInfo';
+import PrivacyPolicyFotter from '../../components/PrivacyPolicyFotter/PrivacyPolicyFotter';
 
 const ShippingAndReturns = () => {
   return (
     <div className='shippingAndReturns'>
         <ShippingAndReturnsHero />
         <ShippingAndReturnsInfo />
+        <PrivacyPolicyFotter />
     </div>
   )
 }
