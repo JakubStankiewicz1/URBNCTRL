ef85cbe50356fd3ed3522462f06aee0cf97fd7fe
Kuba <272264@student.pwr.edu.pl>
2025-06-03 04:54:01 +0200

    Finished Community page

--- frontend/src/pages/Community/Community.jsx
+++ frontend/src/pages/Community/Community.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./community.css";
 import CommunityHeader from '../../components/CommunityHeader/CommunityHeader';
 import CommunityArticle from '../../components/CommunityArticle/CommunityArticle';
+import AboutUsInfoScroll from '../../components/AboutUsInfoScroll/AboutUsInfoScroll';
 
 const Community = () => {
   return (
     <div className='community'>
         <CommunityHeader />
         <CommunityArticle />
+        <AboutUsInfoScroll />
     </div>
   )
 }
