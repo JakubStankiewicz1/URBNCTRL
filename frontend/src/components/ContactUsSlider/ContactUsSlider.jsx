a32b6c76a33b4a3ecc7b99c995f2bd37989a639e
Kuba <272264@student.pwr.edu.pl>
2025-06-03 16:01:37 +0200

    Added ContactUsSlider

---
+++ frontend/src/components/ContactUsSlider/ContactUsSlider.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "./contactUsSlider.css";
+
+import assets from "../../assets/assets";
+
+
+
+const elements = [
+  { src: assets.HomeFeaturedProductsOne, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconOne" },
+  { src: assets.HomeFeaturedProductsTwo, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconTwo" },
+  { src: assets.HomeFeaturedProductsThree, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconThree" },
+  { src: assets.HomeFeaturedProductsFour, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconFour" },
+  { src: assets.HomeFeaturedProductsSix, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconFive" },
+  { src: assets.HomeFeaturedProductsSeven, className: "homeFeaturedProductsContainerDivBottomContainerDivElementContainerIconSix" },
+];
+
+const ContactUsSlider = () => {
+
+
+
+  return (
+    <div className='contactUsSlider'>
+        <div className="contactUsSliderContainer">
+            <div className="contactUsSliderContainerDiv">
+
+
+
+
+
+
+
+
+                {[...elements, ...elements].map((el, idx) => (
+                  <div className="contactUsSliderContainerDivElement" key={idx}>
+                    <div className="contactUsSliderContainerDivElementContainer">
+                      <img src={el.src} alt="" className={el.className} />
+                    </div>
+                  </div>
+                ))}
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ContactUsSlider
--- frontend/src/pages/ContactUs/ContactUs.jsx
+++ frontend/src/pages/ContactUs/ContactUs.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./contactUs.css";
 import ContactUsHero from '../../components/ContactUsHero/ContactUsHero';
 import ContactUsForm from '../../components/ContactUsForm/ContactUsForm';
+import ContactUsSlider from '../../components/ContactUsSlider/ContactUsSlider';
 
 const ContactUs = () => {
   return (
     <div className='contactUs'>
         <ContactUsHero />
         <ContactUsForm />
+        <ContactUsSlider />
     </div>
   )
 }
